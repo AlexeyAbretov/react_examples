08b1b2891701f76f84e4e9c8126b8c607316196a
Alexey Abretov <aleksey.abretov@gazprombank.ru>
2021-05-06 09:26:23 +0300

    feat: added forwardRef example

---
+++ forwardRef/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Box } from './ui';
+
+import {
+    CounterContainer,
+} from 'containers';
+
+export const App = () => (<Box>
+    <CounterContainer />
+</Box>)
